f33ff010ab0367365066077512ba6b5106cb2be0
Mondrya Lawennd <lawennd.e@gmail.com>
2024-09-04 14:05:48 -0300

    Alteracoes nome do metodo.

--- Adopet.API/Controllers/PetController.cs
+++ Adopet.API/Controllers/PetController.cs
@@ -12,8 +12,8 @@ namespace Adopet.API.Controllers
     public class PetController: ControllerBase
     {
         [HttpGet]
-        [Route("GetList")]
-        public async Task<IResult> GetList()
+        [Route("list")]
+        public async Task<IResult> list()
         {
             var _logger = new LoggerConfiguration()
                   .WriteTo.Console()
@@ -40,7 +40,7 @@ namespace Adopet.API.Controllers
 
         [HttpPost]
         [Route("add")]
-        public async Task<IResult> CadatrarPet([FromBody] Pet pet)
+        public async Task<IResult> add([FromBody] Pet pet)
         {
             var configuration = new ConfigurationBuilder()
                                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
